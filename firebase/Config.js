bb52e9e5f4774c964d33a9b8c3b0977c07d79bc0
Roope <roope.nahkala@gmail.com>
2024-02-23 10:54:17 +0200

    valamis

--- firebase/Config.js
+++ firebase/Config.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection,addDoc,serverTimestamp,query,onSnapshot, orderBy } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYOjmzwIv1RgOxpXjdo7JPcL-Vf0ZHKQM",
-  authDomain: "chat-ebe09.firebaseapp.com",
-  projectId: "chat-ebe09",
-  storageBucket: "chat-ebe09.appspot.com",
-  messagingSenderId: "408309197878",
-  appId: "1:408309197878:web:700af8e4110f1d74807c97",
-  measurementId: "G-25FJDWB4HL"
+  
 };
 
 initializeApp(firebaseConfig);
